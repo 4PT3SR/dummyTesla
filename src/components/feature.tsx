698781d37951b21b2d1c1c00d128a42a40ec3485
apt3sr <kingtoluene@gmail.com>
2024-05-19 14:23:01 +0100

    fix: footer into display for mobile

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -6,7 +6,7 @@ type featureProps = {
 const Feature = ({feature,value}:featureProps) => {
   return (
     <p className="flex flex-col items-center justify-center gap-2">
-            <span className="font-extralight">{feature}</span><span className="h-[1px] border-[3px] rounded-full w-[px]"></span><span>{value}</span>
+            <span className="font-extralight">{feature}</span><span className="h-[1px] border-[3px] rounded-full w-[1px]"></span><span>{value}</span>
     </p>
   )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
 import Feature from "./feature"
 const Footer = () => {
   return (
-    <div className=" relative h-16 flex items-center justify-center">
+    <div className="mb-[30px] md:mb-0 relative h-16 flex items-center justify-center">
         <p className="h-[1px] border-collapse border-[1px] w-full md:w-1/2 "></p>
 
         <div className="flex absolute  justify-between w-full md:w-[40%] text-white">
